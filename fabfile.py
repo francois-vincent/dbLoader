c624b6ca1519ec34c42fce22ddae80009e743c16
François Vincent <fvincent@groupeseb.com>
2013-01-08 15:14:13 +0100

    some more refactoring in fabfile

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,7 @@ __github__ = 'https://github.com/francois-vincent'
 from fabric.api import env, task
 from fabric.context_managers import cd, prefix
 from fabric.operations import put, get, run
+from fabric.contrib import files
 from tempfile import NamedTemporaryFile
 import os.path
 from dbMapLoader import mapDict
@@ -18,7 +19,7 @@ WORKING_DIRECTORY = '.dbLoader'
 FabContext = mapDict(
     working_dir = WORKING_DIRECTORY,
     sequencer = 'sequencer.py',
-    data_file = 'files/MyActifry.json',
+    data_file = 'files/MA1.json',
     injection_options = '',
 )
 
@@ -56,31 +57,29 @@ def inject():
         return
     print "will connect to", env.host_string
     # check/create virtualenv '.dbLoader' with subfolder 'files' in ~admin
-    res = run('ls %(working_dir)s/bin/activate' % FabContext, warn_only=True)
-    if res.failed:
+    if not files.exists('%(working_dir)s/bin/activate' % FabContext):
         run('virtualenv %(working_dir)s' % FabContext)
     with cd(WORKING_DIRECTORY):
         #  check/create 'files' subdirectory
-        res = run('ls files', warn_only=True)
-        if res.failed:
+        if not files.exists('files'):
             run('mkdir files')
         with prefix('source bin/activate'):
             # create remote virtualenv python installation (psycopg2, sqlalchemy)
             upload_data2file(dependencies, 'reqs.txt')
             run('pip install -r reqs.txt')
         # upload classes, sequencer and connection parameters
-        put('dbMapLoader.py', 'dbMapLoader.py')
-        put(FabContext.sequencer, FabContext.sequencer)
+        put('dbMapLoader.py', '.')
+        put(FabContext.sequencer, '.')
         upload_data2file(repr(connection), 'connection.json')
         # upload logger
-        put('files/__init__.py', 'files/__init__.py')
-        put('files/simpleLogger.py', 'files/simpleLogger.py')
+        put('files/__init__.py', 'files')
+        put('files/simpleLogger.py', 'files')
         # upload data file
-        put(FabContext.data_file, 'files/'+os.path.basename(FabContext.data_file))
+        put(FabContext.data_file, 'files')
         # launch remote injection command
         with prefix('source bin/activate'):
             run('python %(sequencer)s files/%(data_file)s %(injection_options)s')
-        # download result files (log, flat_inj et flat_seq)
+        # download result files (log, flat_inj and flat_seq)
         res = run('find log -maxdepth 1 -type f', warn_only=True)
         if res and not res.failed:
             local = os.path.join('_host_'+env.host, 'log')
